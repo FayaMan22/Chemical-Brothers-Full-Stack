bc5e81314526e899b0bb2790d856f3d7c7e0c032
Tinashe <tmanchaz@gmail.com>
2026-05-28 23:27:56 +0200

    Fix database URL for Render PostgreSQl

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,12 @@ CORS(app, resources={
     }
 })
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
